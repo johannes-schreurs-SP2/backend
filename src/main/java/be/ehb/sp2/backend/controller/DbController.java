a528945354b866c7aa6f7a3b2de51d1dd4daeff4
JohannesSchreurs <johannes@basedesign.com>
2019-08-27 14:30:53 +0200

    Added survey support on the db controller

--- src/main/java/be/ehb/sp2/backend/controller/DbController.java
+++ src/main/java/be/ehb/sp2/backend/controller/DbController.java
@@ -1,6 +1,8 @@
 package be.ehb.sp2.backend.controller;
 
+import be.ehb.sp2.backend.model.Survey;
 import be.ehb.sp2.backend.model.User;
+import be.ehb.sp2.backend.repository.SurveyRepository;
 import be.ehb.sp2.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +16,18 @@ public class DbController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    SurveyRepository surveyRepository;
+
 
     @GetMapping("/init/users")
     public Iterable<User> initDbUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/init/surveys")
+    public Iterable<Survey> initSurveys() {
+        return surveyRepository.findAll();
     }
 
 
