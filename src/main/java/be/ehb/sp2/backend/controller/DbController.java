57ac82ebae96cab1082d65df05dc60af7dce8f41
JohannesSchreurs <johannes@basedesign.com>
2019-08-27 13:55:28 +0200

    Added a controller for the db to easily query basic models to debug

---
+++ src/main/java/be/ehb/sp2/backend/controller/DbController.java
@@ -0,0 +1,24 @@
+package be.ehb.sp2.backend.controller;
+
+import be.ehb.sp2.backend.model.User;
+import be.ehb.sp2.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/db")
+public class DbController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @GetMapping("/init/users")
+    public Iterable<User> initDbUsers() {
+        return userRepository.findAll();
+    }
+
+
+}
